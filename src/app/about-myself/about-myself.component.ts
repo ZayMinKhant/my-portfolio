2d0b8bf6454a509e39547dd9cab73f766e418074
Zay Min Khant <zmk@mit.com.mm>
2024-03-29 23:34:17 +0630

    latest

---
+++ src/app/about-myself/about-myself.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-myself',
+  templateUrl: './about-myself.component.html',
+  styleUrls: ['./about-myself.component.scss']
+})
+export class AboutMyselfComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMyselfComponent } from './about-myself/about-myself.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMyselfComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
